0c6a22b7a44986d6f07ecab4251ffa36b604320c
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-04 08:23:38 +0700

    add socket config

---
+++ nextjs-socketio/src/app/api/sockets/socketHandler.tsx
@@ -0,0 +1,22 @@
+import { Server } from "socket.io";
+import messageHandler from "../../utils/sockets/messageHandler";
+
+export default function SocketHandler(req: any, res: any) {
+    if (res.socket.server.io) {
+        console.log("Already set up");
+        res.end();
+        return;
+    }
+
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+
+    const onConnection = (socket: any) => {
+        messageHandler(io, socket);
+    };
+
+    io.on("connection", onConnection);
+
+    console.log("Setting up socket");
+    res.end();
+}
