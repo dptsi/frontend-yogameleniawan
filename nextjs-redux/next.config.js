4b6f9b53bce465c1e4d5344708e8c18ffcd2838c
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-10 14:37:12 +0700

    fetching room data

--- nextjs-redux/next.config.js
+++ nextjs-redux/next.config.js
@@ -3,7 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     DB_LOCAL_URI: 'mongodb://localhost:27017/bookit'
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/bookit/image/upload/**',
+      },
+    ],
+    domains: [
+      "res.cloudinary.com"
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ nextjs-redux/src/components/room/RoomItem.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+    room?: any
+}
+
+const RoomItem = ({ room }: Props) => {
+    console.log(room);
+    return (
+        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+            <div className="card p-2">
+                <img
+                    className="card-img-top mx-auto"
+                    src="https://a0.muscache.com/im/pictures/a8f6a489-d236-4d2d-a57b-a95d928970af.jpg?im_w=960"
+                    alt="Card image cap"
+                />
+                <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">
+                        <Link href={`/room/${room._id}`}>{room.name}</Link>
+                    </h5>
+
+                    <div className="ratings mt-auto mb-3">
+                        <p className="card-text"><b>{`$ ${room.pricePerNight}`}</b> / night</p>
+
+                        <div className="rating-outer">
+                            <div className="rating-inner"
+                                style={{ width: `${(room.ratings / 5) * 100}%` }}></div>
+                        </div>
+                        <span id="no_of_reviews">({room.numOfReviews} Reviews)</span>
+                    </div>
+
+                    <div className="btn btn-block view-btn">
+                        <Link href={`/room/${room._id}`}>View Details</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RoomItem;
